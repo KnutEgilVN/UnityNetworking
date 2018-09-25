0ecef45c39027535d461be7b4d6b07d072214588
KnuNyg11 <42740207+KnuNyg11@users.noreply.github.com>
2018-09-25 11:31:21 +0200

    Base code

---
+++ NetworkPlayground/Assets/Scripts/Network/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Assets.Scripts.Network.Models
+{
+    public class Client
+    {
+        //ADD SOCKET IDS
+        public Socket Socket;
+        public List<byte> Data;
+        public byte[] Buffer;
+
+        public Client(Socket socket)
+        {
+            Socket = socket;
+            Data = new List<byte>();
+            Buffer = new byte[1024];
+        }
+    }
+}
---
+++ NetworkPlayground/Assets/Scripts/Network/NetServer.cs
@@ -0,0 +1,85 @@
+﻿using Assets.Scripts.Network.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class NetServer : MonoBehaviour
+{
+    public static NetServer Instance;
+
+    public Socket Server;
+    public int Port;
+
+    public List<Client> Clients;
+
+	void Start ()
+    {
+        Instance = this;
+        Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Clients = new List<Client>();
+
+        Listen();
+	}
+
+    public void Listen()
+    {
+        Server.Bind(new IPEndPoint(IPAddress.Loopback, Port));
+        Server.Listen(8);
+
+        Debug.Log("Server is listening");
+
+        Server.BeginAccept(BeginAcceptCallback, null);
+    }
+    public void Send(Client client, string data)
+    {
+        byte[] buffer = Encoding.UTF8.GetBytes(data);
+        client.Socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, BeginSendCallback, client);
+        Debug.Log("Sending data");
+    }
+
+    void BeginAcceptCallback(IAsyncResult ar)
+    {
+        Client client = new Client(Server.EndAccept(ar));
+        Clients.Add(client);
+
+        Debug.Log("Accepted socket");
+        client.Socket.BeginReceive(client.Buffer, 0, client.Buffer.Length, SocketFlags.None, BeginReceiveCallback, client);
+
+        Server.BeginAccept(BeginAcceptCallback, null);
+    }
+    void BeginReceiveCallback(IAsyncResult ar)
+    {
+        Client client = (Client)ar.AsyncState;
+        int read = client.Socket.EndReceive(ar);
+
+        if(client.Socket.Available > 0)
+        {
+            client.Data.AddRange(client.Buffer);
+            client.Buffer = new byte[client.Buffer.Length];
+
+            client.Socket.BeginReceive(client.Buffer, 0, client.Buffer.Length, SocketFlags.None, BeginReceiveCallback, client);
+            Debug.Log("More data to read");
+        }
+        else
+        {
+            client.Buffer = new byte[client.Buffer.Length];
+            string data = Encoding.UTF8.GetString(client.Data.ToArray());
+            client.Data = new List<byte>();
+
+            //Implement data handling with json
+
+            client.Socket.BeginReceive(client.Buffer, 0, client.Buffer.Length, SocketFlags.None, BeginReceiveCallback, client);
+            Debug.Log("Data read");
+        }
+    }
+    void BeginSendCallback(IAsyncResult ar)
+    {
+        Client client = (Client)ar.AsyncState;
+        int sent = client.Socket.EndSend(ar);
+        Debug.Log("Data sent");
+    }
+}
